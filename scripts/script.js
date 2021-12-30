66d52ac895c82f8eb9632adaf6625fb0f9a55544
Matthew Juliana <matthew.juliana@gmail.com>
2021-12-29 20:39:55 -0500

    build

--- scripts/script.js
+++ scripts/script.js
@@ -143,7 +143,7 @@ function getUploadUrl() {
         "Authorization": idToken
         }})
     .then(response => response.json())
-    .then(response => console.log(response.json()));
+    .then(response => console.log(response));
     // .then(response => uploadFile(response));
 }
 
